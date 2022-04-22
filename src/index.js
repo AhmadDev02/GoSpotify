5602c27f39d73817326693c9fd81a6d3bef4b714
Ahmad Fajar Alfaravi <afajaralfaravi@gmail.com>
2022-04-22 20:42:58 +0700

    perubahan project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import './components/Home/styleThema.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css';
